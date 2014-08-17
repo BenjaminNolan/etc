c316703aaef55f383630130725ca3790bef9b10a
TwoWholeWorms <ben.nolan@gmail.com>
2014-08-17 14:11:45 +0100

    Oh, DUUUUUUUUHHHHH, it has to be a function

--- etc/profile.d/git_prompt.sh
+++ etc/profile.d/git_prompt.sh
@@ -5,16 +5,16 @@
 
 # This adds the active git branch name to the prompt when you cd to a repository on the command line
 
-# function parse_git_branch_and_add_brackets {
-#     git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
-# }
+function parse_git_branch_and_add_brackets {
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
+}
 
 if [ "$PS1" ]; then
     if [ "$BASH" ]; then
         if [ "`id -u`" -eq 0 ]; then
-            export PS1='\[\033[1;31m\]${USER}@${HOSTNAME}\[\033[1;33m\]`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'` \[\033[1;34m\]${PWD/#$HOME/~} $\[\033[0m\] '
+            export PS1='\[\033[1;31m\]${USER}@${HOSTNAME}\[\033[1;33m\]$(parse_git_branch_and_add_brackets) \[\033[1;34m\]${PWD/#$HOME/~} $\[\033[0m\] '
         else
-            export PS1='\[\033[1;32m\]${USER}@${HOSTNAME}\[\033[1;33m\]`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'` \[\033[1;34m\]${PWD/#$HOME/~} $\[\033[0m\] '
+            export PS1='\[\033[1;32m\]${USER}@${HOSTNAME}\[\033[1;33m\]$(parse_git_branch_and_add_brackets) \[\033[1;34m\]${PWD/#$HOME/~} $\[\033[0m\] '
         fi
     else
         if [ "`id -u`" -eq 0 ]; then
