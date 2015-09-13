7935f917c979d8ca1d602fdd88db44aad2d80a92
Benjamin Nolan <ben.nolan@gmail.com>
2015-09-13 22:58:54 +0100

    Added rebase-repo alias

---
+++ etc/profile.d/aliases.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+alias rebase-repo='git checkout --orphan latest_branch && git add -A && git commit -am "Initial commit" && git branch -D master && git branch -m master && git push -f origin master'
+
